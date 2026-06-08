f2bfd30ed2feec6ce07ceb12968edbcd15aa598a
Ebubechukwu Onuorah <ebubeon2022@gmail.com>
2026-06-08 10:07:05 +0100

    Fix migration column order

--- database/migrations/2026_05_13_120939_add_email_verified_to_users_table.php
+++ database/migrations/2026_05_13_120939_add_email_verified_to_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('email_verified')->default(false)->after('role');
+            $table->boolean('email_verified')->default(false);
         });
     }
 
