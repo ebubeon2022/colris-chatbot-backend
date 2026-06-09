637df7edbbf3876469455198bc3d7df28b381426
Ebubechukwu Onuorah <ebubeon2022@gmail.com>
2026-06-09 20:51:16 +0100

    Remove duplicate CORS headers

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, Accept, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
