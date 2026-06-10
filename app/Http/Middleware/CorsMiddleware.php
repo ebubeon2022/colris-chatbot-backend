0954e757f011f20c4f4b160c93a834e8247a64c3
Ebubechukwu Onuorah <ebubeon2022@gmail.com>
2026-06-10 09:03:34 +0100

    Fix CORS for all responses including errors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,6 +13,11 @@ class CorsMiddleware
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
         }
-        return $next($request);
+
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+        return $response;
     }
 }
